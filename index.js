aecb498daa0bcdbf0b7b4cf3967a9c3c25eda8e3
Michael Albertz <michael.albertz@web.de>
2016-07-07 00:04:45 +0200

    compiled files are now also on git

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
  * gulp-strip-ansi - Gulp plugin to strip ansi from files
- * @version v0.1.5
+ * @version v0.2.0
  * @link https://github.com/Prioe/gulp-strip-ansi#readme
  * @license MIT
  */
